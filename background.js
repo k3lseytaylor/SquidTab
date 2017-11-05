7b2e22260c19e788d0f51d29105a6d8544d43f68
K3lseytaylor <mintmochi777@gmail.com>
2017-11-04 21:53:49 -0700

    Changed To local Storage

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.tabs.onCreated.addListener(function(){
+	console.log("created")
+})
--- main.js
+++ main.js
@@ -1,28 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('setcrrnt').addEventListener('click', setonCurrent);
-    document.getElementById('closeBtn').addEventListener('click', pause);
+    document.getElementById('cancelBtn').addEventListener('click', cancelTimeout);
     document.querySelector('.form-1').addEventListener('submit', controller);
-   
+    check();    
 });
 
-var currentTab;
+chrome.tabs.onCreated.addListener(function(tab){
+    
+})
+
 var current;
 var currentID;
 
 
-function open(website){
-	var url = JSON.parse(website);
-    chrome.tabs.create({'url': 'https://' + url}, function(tab) {
-    	    chrome.tabs.onCreated.addListener(function(tab){
-    			console.log("new tab "+tab.id);
-    			currentTab = tab.id;
-	});
-  }); 
-}
-
-function close(){
-	setTimeout(function(){ chrome.tabs.remove(currentTab); }, 3000);
-}
 
 function controller(e){
   	const address = JSON.stringify((this.querySelector('[name=page]')).value);
@@ -30,39 +20,60 @@ function controller(e){
   	//make time goes as minutes
   	console.log(time);
   	console.log(address);
-  	timeCtrl(time)
   	goTo(address,time);
   	e.preventDefault();
   	this.reset();
  }
 
 
- function goTo(website,time){
-	var url = JSON.parse(website);
+function goTo(website,time){
+  var url = JSON.parse(website);
+  const box = {
+    id:null,
+    timeout:time
+  }
     chrome.tabs.create({'url': 'https://' + url}, function(tab) {
-    	console.log('working');
-    	current = setTimeout(function(){ chrome.tabs.remove(tab.id); }, time);
+      box.id = tab.id;
+      console.log('working', box);
+       current = setTimeout(function(){ chrome.tabs.remove(tab.id); }, time);
+
+      chrome.storage.sync.set({'value': box}, function() { 
+          console.log('Settings saved');
+        }); 
   }); 
-}
-
-function pause(){
-	 clearTimeout(current);
-	 console.log('paused');
-}
-
-function timeCtrl(time){
-	newTime = time * 60
-	console.log(newTime);
 }
 
 function setonCurrent(){
 	const time = (document.getElementById('time-2')).value * 1000
 	console.log(time);
+
 	document.getElementById("form-2").reset();
 	chrome.tabs.query({active:true,windowType:"normal", currentWindow: true},
-		function(d){
-			console.debug(d[0].id);
-			currentID=d[0].id;})
+		function(tab){
+			currentID = tab[0].id;
+      console.log(currentID);
+  })
 	current = setTimeout(function(){ chrome.tabs.remove(currentID); }, time);
-
 }
+
+function check(){
+   chrome.storage.sync.get(["value"], function(items){
+    console.log(items);
+     if(Object.keys(items).length === 0){
+      console.log('nothing');
+        return;
+      }else{
+      chrome.storage.sync.clear(function(){
+       console.log('cleared') 
+     });
+      current = setTimeout(function(){ chrome.tabs.remove(items.value.id); }, items.value.timeout);
+      } 
+  }); 
+}
+
+function cancelTimeout(){
+   clearTimeout(current);
+   console.log('paused');
+}
+
+
